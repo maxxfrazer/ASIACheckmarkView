28262fe026246bf6c63c1d97b2450513c4eb7eae
Andrzej Michnia <amichnia@pgs-soft.com>
2016-03-13 11:30:49 +0100

    Initial Commit

---
+++ ASIACheckmarkView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ASIACheckmarkView
+//
+//  Created by Andrzej Michnia on 13.03.2016.
+//  Copyright © 2016 Andrzej Michnia Usługi Programistyczne. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
